4b5991d9acf776659b05bd122e27595b4b1ac3f7
ywcho <ywcho0328@gmail.com>
2022-12-24 17:16:08 -0800

    initiall update for test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as gpio
+
+gpio.setmode(gpio.BCM)
+
+LED_pin = 2
+sw_pin = 17
+gpio.setup(LED_pin, gpio.OUT)
+gpio.setup(sw_pin, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+
+try:
+    while True:
+        if gpio.input(sw_pin) == gpio.HIGH:
+            gpio.output(LED_pin, gpio.HIGH)
+            print('button pressed!')
+        else:
+            gpio.output(LED_pin, gpio.LOW)
+
+finally:
+    gpio.cleanup()
